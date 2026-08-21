10ab006ced1517d392578265d59550a61b1c97ae
Ranjeet Singh <ranjeetsinghazad11@gmail.com>
2026-08-21 18:00:20 +0530

    4nd commit

--- module/02_vnet/main.tf
+++ module/02_vnet/main.tf
@@ -1,8 +1,8 @@
-resource "azurerm_virtual_network" "main" {
-  name                = var.vnet_name
-  address_space       = [var.vnet_address_space]
-  location            = var.location
-  resource_group_name = var.resource_group_name
+# resource "azurerm_virtual_network" "main" {
+#   name                = var.vnet_name
+#   address_space       = [var.vnet_address_space]
+#   location            = var.location
+#   resource_group_name = var.resource_group_name
 
-  tags = var.tags
-}
+#   tags = var.tags
+# }
